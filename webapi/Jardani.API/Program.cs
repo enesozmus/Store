96f4419b90844aa1ec8bedb08180cbcbe9bfdbf4
enesozmus <ensozmus@gmail.com>
2024-12-31 14:21:51 +0300

    Infrastructure Layer | Setting Up MSSQL

--- webapi/Jardani.API/Program.cs
+++ webapi/Jardani.API/Program.cs
@@ -1,7 +1,14 @@
+using Jardani.Infrastructure.EFCore.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 {
     builder.Services.AddControllers();
+    builder.Services.AddDbContext<StoreContext>(opt =>
+    {
+        opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    });
     builder.Services.AddEndpointsApiExplorer();
 }
 
