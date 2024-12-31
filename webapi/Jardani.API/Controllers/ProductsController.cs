d3d59cffd98caf4780b3f76a7fec93c18e210c0c
enesozmus <ensozmus@gmail.com>
2024-12-31 14:36:12 +0300

    Creating a Products Controller and Using Postman to Test Our New Api Endpoints

---
+++ webapi/Jardani.API/Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+using Jardani.Domain.Entities;
+using Jardani.Infrastructure.EFCore.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jardani.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly StoreContext _context;
+
+    public ProductsController(StoreContext context)
+        => this._context = context;
+
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts()
+        => await _context.Products.ToListAsync();
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product == null) return NotFound();
+        return product;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Product>> CreateProduct(Product product)
+    {
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+        return product;
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> UpdateProduct(int id, Product product)
+    {
+        if (product.Id != id || !ProductExists(id))
+            return BadRequest("Cannot update this product");
+
+        _context.Entry(product).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return Ok();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeleteProduct(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product == null) return NotFound();
+
+        _context.Products.Remove(product);
+        await _context.SaveChangesAsync();
+        return Ok();
+    }
+
+    private bool ProductExists(int id)
+        => _context.Products.Any(x => x.Id == id);
+}
