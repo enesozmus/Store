68cfaf18085fcf253db428540be2a6ad9a81ce5a
enesozmus <ensozmus@gmail.com>
2024-12-31 15:15:55 +0300

    Customized SaveChangesAsync Interceptor

--- webapi/Jardani.Domain/Entities/Common/BaseEntity.cs
+++ webapi/Jardani.Domain/Entities/Common/BaseEntity.cs
@@ -3,4 +3,6 @@ namespace Jardani.Domain.Entities;
 public class BaseEntity
 {
     public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
 }
--- webapi/Jardani.Infrastructure/EFCore/Contexts/StoreDbContext.cs
+++ webapi/Jardani.Infrastructure/EFCore/Contexts/StoreDbContext.cs
@@ -19,4 +19,21 @@ public class StoreDbContext : DbContext
         base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfigurations).Assembly);
     }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedAt = DateTime.Now;
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.UpdatedAt = DateTime.Now;
+                    break;
+            }
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
 }
