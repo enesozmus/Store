ffca1bd229bd5ba235db676b3520201204e81a44
enesozmus <ensozmus@gmail.com>
2025-02-22 18:03:32 +0300

    Adding a Header Component-3 | header-nav

--- client/src/app/ui/header/header.component.ts
+++ client/src/app/ui/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderTopComponent } from './header-top/header-top.component';
+import { HeaderNavComponent } from './header-nav/header-nav.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [HeaderTopComponent],
+  imports: [HeaderTopComponent, HeaderNavComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
