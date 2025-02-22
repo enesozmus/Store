648f12ed99d5706a3af03349d8b105ca4957e139
enesozmus <ensozmus@gmail.com>
2025-02-22 19:51:02 +0300

    Adding a Home Component-2 | brand-container

---
+++ client/src/app/ui/home/brand-container/brand-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brand-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './brand-container.component.html',
+  styleUrl: './brand-container.component.scss',
+})
+export class BrandContainerComponent {}
--- client/src/app/ui/home/home.component.ts
+++ client/src/app/ui/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SliderComponent } from './slider/slider.component';
+import { BrandContainerComponent } from "./brand-container/brand-container.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SliderComponent],
+  imports: [SliderComponent, BrandContainerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
