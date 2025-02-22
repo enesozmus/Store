b0c546293fd0442c806950bbb3aef7e417b1e41d
enesozmus <ensozmus@gmail.com>
2025-02-22 19:47:41 +0300

    Adding a Home Component-1 | slider

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './ui/header/header.component';
+import { HomeComponent } from "./ui/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,6 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// import function to register Swiper custom elements
+import { register as registerSwiperElements } from 'swiper/element/bundle';
+// register Swiper custom elements
+registerSwiperElements();
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
