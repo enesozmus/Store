c97eeb5fc0d9e7be43dc9f45711e561603d63ad2
enesozmus <ensozmus@gmail.com>
2025-02-22 18:54:07 +0300

    Adding a Header Component-4 | header-main

---
+++ client/src/app/ui/header/header-main/header-main.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-main.component.html',
+  styleUrl: './header-main.component.scss',
+})
+export class HeaderMainComponent {
+  showMenuButton = false;
+  menuSwitch = false;
+
+  onToggleMenu() {
+    this.menuSwitch = !this.menuSwitch;
+  }
+}
--- client/src/app/ui/header/header.component.ts
+++ client/src/app/ui/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderTopComponent } from './header-top/header-top.component';
 import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { HeaderMainComponent } from './header-main/header-main.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [HeaderTopComponent, HeaderNavComponent],
+  imports: [HeaderTopComponent, HeaderNavComponent, HeaderMainComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
