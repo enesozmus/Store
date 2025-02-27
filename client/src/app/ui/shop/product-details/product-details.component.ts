e6d47eee95a9d6bc3ffa05b29d263a741364cb84
enesozmus <ensozmus@gmail.com>
2025-02-27 11:36:48 +0300

    Designing the Product Details Page

--- client/src/app/ui/shop/product-details/product-details.component.ts
+++ client/src/app/ui/shop/product-details/product-details.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
 import { ShopService } from '../../../services/shop.service';
 import { Product } from '../../../shared/models/product';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ProductDetailsComponent {
   product?: Product;
+
+  information = false;
+  detail = false;
+  custom = false;
+  review = false;
 
   constructor(
     private shopService: ShopService,
@@ -33,5 +40,22 @@ export class ProductDetailsComponent {
         console.error(error);
       },
     });
+  }
+
+  onHandleAddToCart() {}
+  onHandleDecreaseByOne() {}
+  onHandleIncreaseByOne() {}
+
+  onOpenInformation() {
+    this.information = !this.information;
+  }
+  onOpenDetail() {
+    this.detail = !this.detail;
+  }
+  onOpenCustom() {
+    this.custom = !this.custom;
+  }
+  onOpenReview() {
+    this.review = !this.review;
   }
 }
