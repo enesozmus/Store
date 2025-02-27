ddd61caad31d4db9e66e3bbf30e395e0b7fdcd2f
enesozmus <ensozmus@gmail.com>
2025-02-27 12:46:10 +0300

    Adding a NotFound and ServerError Component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,6 +4,10 @@ import { HomeComponent } from './ui/home/home.component';
 import { ShopComponent } from './ui/shop/shop.component';
 import { ProductDetailsComponent } from './ui/shop/product-details/product-details.component';
 
+import { TestErrorComponent } from './shared/test-error/test-error.component';
+import { NotFoundComponent } from './shared/components/errors/not-found/not-found.component';
+import { ServerErrorComponent } from './shared/components/errors/server-error/server-error.component';
+
 export const routes: Routes = [
   {
     path: '',
@@ -18,8 +22,25 @@ export const routes: Routes = [
     component: ProductDetailsComponent,
   },
   {
+    path: 'test-error',
+    component: TestErrorComponent,
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+  },
+  {
+    path: 'server-error',
+    component: ServerErrorComponent,
+  },
+  // {
+  //   path: '**',
+  //   redirectTo: '',
+  //   pathMatch: 'full',
+  // },
+  {
     path: '**',
-    redirectTo: '',
+    redirectTo: 'not-found',
     pathMatch: 'full',
   },
 ];
---
+++ client/src/app/shared/test-error/test-error.component.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-test-error',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-error.component.html',
+  styleUrl: './test-error.component.scss',
+})
+export class TestErrorComponent {
+  // baseUrl = environment.apiUrl;
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'buggy/notfound').subscribe({
+      next: (response) => console.log(response),
+      error: (error) => console.log(error),
+    });
+  }
+
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/badrequest').subscribe({
+      next: (response) => console.log(response),
+      error: (error) => console.log(error),
+    });
+  }
+
+  get401Error() {
+    this.http.get(this.baseUrl + 'buggy/unauthorized').subscribe({
+      next: (response) => console.log(response),
+      error: (error) => console.log(error),
+    });
+  }
+
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/internalerror').subscribe({
+      next: (response) => console.log(response),
+      error: (error) => console.log(error),
+    });
+  }
+}
