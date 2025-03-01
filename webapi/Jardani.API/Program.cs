32e3666e5b0d1962e16ee76beae5f1833feed767
enesozmus <ensozmus@gmail.com>
2025-03-02 00:56:10 +0300

    Creating a Cart Service

--- webapi/Jardani.API/Program.cs
+++ webapi/Jardani.API/Program.cs
@@ -1,8 +1,10 @@
 using Jardani.API.Exceptions.ExceptionHandlers;
 using Jardani.API.Middlewares;
+using Jardani.Application.IServices;
 using Jardani.Infrastructure;
 using Jardani.Infrastructure.EFCore.Contexts;
 using Jardani.Infrastructure.EFCore.Seeds;
+using Jardani.Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using StackExchange.Redis;
 
@@ -20,12 +22,13 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddProblemDetails();
     builder.Services.AddCors();
     builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
-{
-    var connString = builder.Configuration.GetConnectionString("Redis")
-        ?? throw new Exception("Cannot get redis connection string");
-    var configuration = ConfigurationOptions.Parse(connString, true);
-    return ConnectionMultiplexer.Connect(configuration);
-});
+    {
+        var connString = builder.Configuration.GetConnectionString("Redis")
+            ?? throw new Exception("Cannot get redis connection string");
+        var configuration = ConfigurationOptions.Parse(connString, true);
+        return ConnectionMultiplexer.Connect(configuration);
+    });
+    builder.Services.AddSingleton<ICartService, CartService>();
 }
 
 var app = builder.Build();
---
+++ webapi/Jardani.Application/IServices/ICartService.cs
@@ -0,0 +1,10 @@
+using Jardani.Domain.Entities;
+
+namespace Jardani.Application.IServices;
+
+public interface ICartService
+{
+    Task<ShoppingCart?> GetCartAsync(string key);
+    Task<ShoppingCart?> SetCartAsync(ShoppingCart cart);
+    Task<bool> DeleteCartAsync(string key);
+}
