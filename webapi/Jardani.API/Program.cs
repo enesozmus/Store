59e888874e3595e8f522c7752620d3ba67aceb08
enesozmus <ensozmus@gmail.com>
2025-03-02 00:45:02 +0300

    Using Redis via Docker with .NET

--- webapi/Jardani.API/Program.cs
+++ webapi/Jardani.API/Program.cs
@@ -4,6 +4,7 @@ using Jardani.Infrastructure;
 using Jardani.Infrastructure.EFCore.Contexts;
 using Jardani.Infrastructure.EFCore.Seeds;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,13 +19,24 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
     builder.Services.AddProblemDetails();
     builder.Services.AddCors();
+    builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
+{
+    var connString = builder.Configuration.GetConnectionString("Redis")
+        ?? throw new Exception("Cannot get redis connection string");
+    var configuration = ConfigurationOptions.Parse(connString, true);
+    return ConnectionMultiplexer.Connect(configuration);
+});
 }
 
 var app = builder.Build();
 
 {
     app.UseExceptionHandler(opt => { });
-    app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:4200","https://localhost:4200"));
+    app.UseCors(
+        x => x.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .WithOrigins("http://localhost:4200", "https://localhost:4200"));
     app.UseHttpsRedirection();
 
     app.UseAuthorization();
