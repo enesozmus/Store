25aae69586f0ef88b39cbdbebe82e7c41509c883
enesozmus <ensozmus@gmail.com>
2025-03-03 13:59:13 +0300

    Persisting the Cart

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,8 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorInterceptor } from './shared/interceptors/error.interceptor';
+
+import { InitService } from './services/init.service';
+import { lastValueFrom } from 'rxjs';
 
 import { routes } from './app.routes';
 
@@ -13,12 +20,28 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 import { loadingInterceptor } from './shared/interceptors/loading.interceptor';
 
+
+function initializeApp(initService: InitService) {
+  return () => lastValueFrom(initService.init()).finally(() => {
+    const splash = document.getElementById('initial-splash');
+    if (splash) {
+      splash.remove();
+    }
+  })
+}
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([errorInterceptor])),
-    // provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor])),
+    // provideHttpClient(withInterceptors([errorInterceptor])),
+    provideHttpClient(withInterceptors([errorInterceptor, loadingInterceptor])),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      deps: [InitService],
+      multi: true,
+    },
     provideAnimations(),
     provideToastr(),
   ],
---
+++ client/src/app/services/init.service.ts
@@ -0,0 +1,18 @@
+import { inject, Injectable } from '@angular/core';
+import { CartService } from './cart.service';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InitService {
+  private cartService = inject(CartService);
+
+  init() {
+    const cartId = localStorage.getItem('cart_id');
+    // console.log('🟥Init', cartId);
+    const cart$ = cartId ? this.cartService.getCart(cartId) : of(null);
+
+    return cart$;
+  }
+}
