465191b767b3fe49303f6f57190ef8d1ba58b8f2
enesozmus <ensozmus@gmail.com>
2025-03-07 15:05:09 +0300

    Adding a Custom Register Endpoint

---
+++ webapi/Jardani.API/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+using Jardani.API.DTOs;
+using Jardani.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jardani.API.Controllers;
+public class AccountController(SignInManager<AppUser> signInManager) : BaseApiController
+{
+    [HttpPost("register")]
+    public async Task<ActionResult> Register(RegisterDto registerDto)
+    {
+        var user = new AppUser
+        {
+            FirstName = registerDto.FirstName,
+            LastName = registerDto.LastName,
+            Email = registerDto.Email,
+            UserName = registerDto.Email
+        };
+
+        var result = await signInManager.UserManager.CreateAsync(user, registerDto.Password);
+
+        if (!result.Succeeded)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(error.Code, error.Description);
+            }
+
+            return ValidationProblem();
+        }
+        return Ok();
+    }
+}
