b11214121e701c559f81321da48adcfce8ffa639
enesozmus <ensozmus@gmail.com>
2025-03-12 13:14:16 +0300

    Adding the Register Form

--- client/src/app/ui/account/register/register.component.ts
+++ client/src/app/ui/account/register/register.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { JsonPipe } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from '../../../services/account.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  private formBuilder = inject(FormBuilder);
+  private accountService = inject(AccountService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
+
+  registerForm = this.formBuilder.group({
+    firstName: [''],
+    lastName: [''],
+    email: [''],
+    password: [''],
+  });
+
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: () => {
+        this.toastr.success('Registration successful - you can now login', '', {
+          progressBar: true,
+          timeOut: 5000,
+        });
+        this.router.navigateByUrl('/account/login');
+      },
+    });
+  }
+}
